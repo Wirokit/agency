ef8c2244443aaf5ce3b31ff2bc62fba6ee870141
Sami Pitkänen <sami.pitkanen@wirokit.com>
2026-04-13 11:23:05 +0300

    Small fix

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -70,7 +70,7 @@ def check_pin():
 
     session["pin_user"] = result["data_owner"]
     session["pin_code"] = request.values["pin"]
-    session["cv_id"] = str(request.values["id"])
+    session["cv_id"] = str(result["id"])
 
     return jsonify({"success": True, "data": result})
 
