b2a242f89c2a3cd4fab4ad4f8dd070b92405215e
Sami Pitkänen <sami.pitkanen@wirokit.com>
2026-04-14 12:45:56 +0300

    Gunicorn fix

--- application.py
+++ application.py
@@ -2,8 +2,9 @@ import os
 from app import create_app
 from config import getConfig
 
+application = create_app(config=getConfig())
+
 if __name__ == "__main__":
     # Run the app.
     # 'debug=True' is great for development as it auto-reloads.
-    application = create_app(config=getConfig())
     application.run(debug=os.environ.get("DEBUG_MODE") == "TRUE", host="0.0.0.0")
